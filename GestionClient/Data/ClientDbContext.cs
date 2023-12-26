b0953757addb800cbfd57837d7e82150b259a866
4iir2024 <4iir2024@gmail.com>
2023-12-26 19:13:41 +0100

    TP First Access DB

---
+++ GestionClient/Data/ClientDbContext.cs
@@ -0,0 +1,11 @@
+﻿using GestionClient.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionClient.Data
+{
+    public class ClientDbContext:DbContext
+    {
+        public ClientDbContext(DbContextOptions<ClientDbContext> options) : base(options) { }
+        public DbSet<Client> Clients { get; set; }
+    }
+}
---
+++ GestionClient/Migrations/20231226085745_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestionClient.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Prenom = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ville = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clients");
+        }
+    }
+}
